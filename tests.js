f713e03ca2654d3d499bdc5ea31244ddf3fc5261
Mariano Iglesias <mariano@cricava.com>
2011-10-23 13:46:40 -0300

    Allowing users to change precision. Fixing and adding tests

--- tests.js
+++ tests.js
@@ -1,8 +1,15 @@
 /* Escape & Query building tests */
 
+var settings = JSON.parse(require('fs').readFileSync('./tests-settings.json','utf8'));
+
 var mysql = require("./db-mysql");
-var tests = require("./lib/node-db/tests.js").get(function() {
-    return new mysql.Database();
+var tests = require("./lib/node-db/tests.js").get(function(callback) {
+    new mysql.Database(settings).connect(function(err) {
+        if (err) {
+            throw new Error('Could not connect to test DB');
+        }
+        callback(this);
+    });
 });
 
 for(var test in tests) {
